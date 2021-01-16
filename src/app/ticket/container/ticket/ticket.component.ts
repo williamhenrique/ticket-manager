0e4bb80f2916fc1a77a55f9b4fc3276e8917a19a
William Henrique Lima Da Silva <william.lima@totvs.com.br>
2021-01-16 20:14:34 -0300

    criado formulario

---
+++ src/app/ticket/container/ticket/ticket.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.scss']
+})
+export class TicketComponent implements OnInit {
+  form = new FormGroup({
+    ticket: new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required])
+    }),
+    assign: new FormGroup({
+      user_id: new FormControl('123')
+    }),
+    comment: new FormGroup({
+      text: new FormControl(''),
+      comments: new FormArray([])
+    })
+  })
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  add(){
+    const text = this.form.get('comment.text') as FormArray;
+    const comments = this.form.get('comment.comments') as FormArray;
+    comments.push(this.createComment(text.value));
+    text.reset()
+  }
+
+  createComment(value){
+    return  new FormGroup({
+      comment_text: new FormControl(value),
+    })
+  }
+
+  remove(index){
+    const control = this.form.get('comment.comments') as FormArray;
+    control.removeAt(index);
+  }
+
+}
