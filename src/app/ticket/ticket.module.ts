f8cda82ad214e3326592fc5b912d84c93492f080
William Henrique Lima Da Silva <william.lima@totvs.com.br>
2021-01-18 11:03:46 -0300

    finalizando branch

--- src/app/ticket/ticket.module.ts
+++ src/app/ticket/ticket.module.ts
@@ -7,16 +7,13 @@ import { ListComponent } from './components/list/list.component';
 import { GridComponent } from './components/grid/grid.component';
 
 import { HomeComponent } from './container/home/home.component';
-import { TicketComponent } from './container/ticket/ticket.component';
-import { PoModule } from '@po-ui/ng-components';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'new', component: TicketComponent}
 ];
 
 @NgModule({
-  declarations: [HomeComponent, ListComponent, GridComponent, TicketComponent],
+  declarations: [HomeComponent, ListComponent, GridComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
