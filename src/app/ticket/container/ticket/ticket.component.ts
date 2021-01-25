2b091f55948869321c23deac617f275b2509116a
William <william.henrique@live.com>
2021-01-25 18:02:29 -0300

    feat: criando validação formulario

--- src/app/ticket/container/ticket/ticket.component.ts
+++ src/app/ticket/container/ticket/ticket.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket',
@@ -7,40 +7,73 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent implements OnInit {
-  form = new FormGroup({
-    ticket: new FormGroup({
-      title: new FormControl('', [Validators.required]),
-      description: new FormControl('', [Validators.required])
+  form = this.fb.group({
+    ticket: this.fb.group({
+      title: this.fb.control('', [Validators.email]),
+      description: this.fb.control('', [Validators.required])
     }),
-    assign: new FormGroup({
-      user_id: new FormControl('123')
+    assign: this.fb.group({
+      user_id: this.fb.control('123')
     }),
-    comment: new FormGroup({
-      text: new FormControl(''),
-      comments: new FormArray([])
+    comment: this.fb.group({
+      text: this.fb.control('', [Validators.required]),
+      comments: this.fb.array([])
     })
   })
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form.valueChanges.subscribe(() =>{
+      console.log(this.form.touched)
+    })
   }
 
   add(){
-    const text = this.form.get('comment.text') as FormArray;
+    const text = this.form.get('comment.text') as FormControl;
     const comments = this.form.get('comment.comments') as FormArray;
     comments.push(this.createComment(text.value));
     text.reset()
+
   }
 
   createComment(value){
-    return  new FormGroup({
-      comment_text: new FormControl(value),
+    return  this.fb.group({
+      comment_text: this.fb.control(value),
     })
   }
 
   remove(index){
     const control = this.form.get('comment.comments') as FormArray;
     control.removeAt(index);
+  }
+
+  resetForm(){
+    this.form.reset();
+    // // this.form.setValue({
+    // //   ticket: {
+    // //     title: '',
+    // //     description: ''
+    // //   },
+    // //   assign: {
+    // //     user_id: ''
+    // //   },
+    // //   comment: {
+    // //     text: '',
+    // //     comments: []
+    // //   }
+    // // })
+    // this.form.patchValue({
+    //   ticket: {
+    //     title: ''
+    //   }
+    // })
+  }
+
+  required(field: string, form: string){
+    return (
+      this.form.get(`${form}.${field}`).hasError('required') &&
+      (this.form.get(`${form}.${field}`).touched || this.form.get(`${form}.${field}`).dirty)
+    )
   }
 
 }
